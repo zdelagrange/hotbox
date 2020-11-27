6bd2294135bd72ee12612e0394b9262b1f140e99
zac <zac.delagrange@gmail.com>
2020-11-27 17:31:22 -0500

    add shebang

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 go get github.com/d2r2/go-dht
 go get github.com/gorilla/mux
 go get github.com/bitly/go-simplejson
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ./cmd/crons/main > ./crons.log 2>&1 &
 ./cmd/api/main > ./api.log 2>&1 &
 tail -f *.log
