49a1ba2339cdbd7f77f9a0e12107b6fea827bfe9
zac <zac.delagrange@gmail.com>
2020-11-29 17:24:18 -0500

    add readings endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
 
 	"log"
 	"net/http"
@@ -17,7 +21,7 @@ type reading struct {
 	Humidity    float32
 }
 
-// Reading GET latest reading
+// Reading GET latest reading; POST new reading
 func Reading(w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("sqlite3", "./hotbox.db")
 	checkErr(err)
@@ -51,6 +55,88 @@ func Reading(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Readings GET range of readings
+func Readings(w http.ResponseWriter, r *http.Request) {
+	var idays int
+	var ihours int
+	var iminutes int
+	var iseconds int
+	now := time.Now()
+
+	query := r.URL.Query()
+	negativeSign := "-"
+	negativeSign = string(negativeSign)
+	days, ok := query["days"]
+	if !ok || len(days) < 1 {
+		http.Error(w, "malformed url", http.StatusBadRequest)
+	} else if len(days) > 0 {
+		idays = convertQueryString(days)
+	} else {
+		idays = 1
+	}
+	now.AddDate(0, 0, idays)
+	hours, ok := query["hours"]
+	if !ok {
+		http.Error(w, "malformed url", http.StatusBadRequest)
+	} else if len(hours) > 0 {
+		ihours = convertQueryString(hours)
+	} else {
+		ihours = 0
+	}
+	now.Add(time.Duration(ihours))
+	minutes, ok := query["minutes"]
+	if !ok {
+		http.Error(w, "malformed url", http.StatusBadRequest)
+	} else if len(minutes) > 0 {
+		iminutes = convertQueryString(minutes)
+	} else {
+		iminutes = 0
+	}
+	now.Add(time.Duration(iminutes))
+	seconds, ok := query["seconds"]
+	if !ok || len(seconds) < 1 {
+		http.Error(w, "malformed url", http.StatusBadRequest)
+	} else if len(seconds) > 0 {
+		iseconds = convertQueryString(seconds)
+	} else {
+		iseconds = 0
+	}
+	now.Add(time.Duration(iseconds))
+
+	var (
+		humidity    string
+		temperature string
+	)
+	var readings []*reading
+	db, err := sql.Open("sqlite3", "./hotbox.db")
+	checkErr(err)
+
+	rows, err := db.Query(fmt.Sprintf("SELECT humidity, temperature FROM reading WHERE datetime > '%s';", now.String()))
+	checkErr(err)
+	defer rows.Close()
+	for rows.Next() {
+		err = rows.Scan(&humidity, &temperature)
+		newReading := new(reading)
+		fhumidity, _ := strconv.ParseFloat(humidity, 32)
+		ftemperature, _ := strconv.ParseFloat(temperature, 32)
+		newReading.Humidity = float32(fhumidity)
+		newReading.Temperature = float32(ftemperature)
+		readings = append(readings, newReading)
+	}
+	payload, _ := json.Marshal(readings)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(payload)
+}
+
+func convertQueryString(original []string) int {
+	negativeSign := "-"
+	negativeSign = string(negativeSign)
+	new := strings.Join(original, "")
+	new = negativeSign + new
+	newInt, _ := strconv.Atoi(new)
+	return newInt
+}
+
 func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
@@ -64,7 +150,7 @@ func main() {
 	})
 
 	router.HandleFunc("/api/reading", Reading).Methods("GET", "POST")
-	// router.HandleFunc("/api/readings", Readings).Methods("GET")
+	router.HandleFunc("/api/readings", Readings).Methods("GET")
 
 	handler := c.Handler(router)
 
